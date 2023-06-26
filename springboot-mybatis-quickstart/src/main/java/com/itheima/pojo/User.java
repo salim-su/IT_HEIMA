9482da1b6524d01d8bbb28c1c0aac8eb46de6053
salim-su <salimsucc@gmail.com>
2023-06-26 16:12:57 +0800

    salim

--- springboot-mybatis-quickstart/src/main/java/com/itheima/pojo/User.java
+++ springboot-mybatis-quickstart/src/main/java/com/itheima/pojo/User.java
@@ -1,75 +1,13 @@
 package com.itheima.pojo;
 
+import lombok.Data;
+
+@Data
 public class User {
     private Integer id;
     private String name;
     private short age;
     private short gender;
     private String phone;
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", gender=" + gender +
-                ", phone='" + phone + '\'' +
-                '}';
-    }
-
-    public User(Integer id, String name, short age, short gender, String phone) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.phone = phone;
-    }
-
-    public User() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public short getAge() {
-        return age;
-    }
-
-    public void setAge(short age) {
-        this.age = age;
-    }
-
-    public short getGender() {
-        return gender;
-    }
-
-    public void setGender(short gender) {
-        this.gender = gender;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-
-
 }
 
